fd0379f4a5a14e317a85391ed342b064e82d34fc
webreflection <andrea.giammarchi@gmail.com>
2024-12-05 17:36:37 +0100

    Lazy all the things

--- src/main.js
+++ src/main.js
@@ -17,20 +17,30 @@ import {
   withResolvers,
 } from './shared.js';
 
-const CHANNEL = crypto.randomUUID();
+let {
+  BigInt64Array,
+  Int32Array,
+  SharedArrayBuffer,
+  SharedWorker,
+  Worker,
+} = globalThis;
 
-const { defineProperties } = Object;
+let CHANNEL = '';
+let ignore = ignoreDirect;
+let polyfill = false;
 
-const { notify, waitAsync } = Atomics;
+let { notify, waitAsync } = Atomics;
+if (!waitAsync) waitAsync = waitAsyncPatch;
+Atomics.notify = (...args) => notify(...args);
+Atomics.waitAsync = (...args) => waitAsync(...args);
 
 const sync = new Map;
-const once = { once: true };
-
-const asModule = options => ({ ...options, type: 'module' });
 
 const addListener = (self, type, handler, ...rest) => {
   self.addEventListener(type, handler, ...rest);
 };
+
+const asModule = options => ({ ...options, type: 'module' });
 
 const message = event => {
   if (isChannel(event, CHANNEL)) {
@@ -48,73 +58,36 @@ const message = event => {
   }
 };
 
-const register = ({ serviceWorker: s }, sw, done) => {
-  let w, c = true;
-  addListener(s, 'message', event => {
-    if (isChannel(event, CHANNEL)) {
-      const [_, id, index] = event.data;
-      const uid = [id, index].join(',');
-      const done = view => {
-        sync.delete(uid);
-        w.postMessage([ CHANNEL, id, index, view ]);
-      };
-      const view = sync.get(uid);
-      if (view) done(view);
-      else {
-        const { promise, resolve } = withResolvers();
-        sync.set(uid, resolve);
-        promise.then(done);
+const patchAtomics = () => {
+  if (!CHANNEL) {
+    CHANNEL = crypto.randomUUID();
+
+    // partial patches for the SharedWorker only case
+    // Workers with native SharedArrayBuffer should not
+    // get affected much or penalized, performance wise
+    const [$notify, $waitAsync] = [notify, waitAsync];
+
+    notify = (view, index, ...rest) => {
+      const data = getData(view);
+      if (data) {
+        const [id, worker] = data;
+        const uid = [id, index].join(',');
+        const known = sync.get(uid);
+        if (known) known(view);
+        else sync.set(uid, view);
+        worker.postMessage([CHANNEL, ACTION_NOTIFY, view, id, index]);
+        return 0;
       }
-    }
-  });
-  // use previous registration, if any, before registering it
-  s.getRegistration(sw)
-    .then(r => (r ?? s.register(sw)))
-    .then(function ready(r) {
-      c = c && !!s.controller;
-      w = (r.installing || r.waiting || r.active);
-      if (w.state === 'activated') {
-        if (c) done();
-        else location.reload();
+      else return $notify(view, index, ...rest);
+    };
+  
+    waitAsync = (view, ...rest) => {
+      if (views.has(view)) {
+        const [_, value] = waitAsyncPoly(view, ...rest);
+        return { value };
       }
-      else
-        addListener(w, 'statechange', () => ready(r), once);
-    });
-};
-
-let {
-  BigInt64Array,
-  Int32Array,
-  SharedArrayBuffer,
-  SharedArrayBuffer: SAB,
-  SharedWorker,
-  Worker,
-} = globalThis;
-
-let ignore = ignoreDirect;
-let polyfill = false;
-
-Atomics.notify = (view, index, ...rest) => {
-  const data = getData(view);
-  if (data) {
-    const [id, worker] = data;
-    const uid = [id, index].join(',');
-    const known = sync.get(uid);
-    if (known) known(view);
-    else sync.set(uid, view);
-    worker.postMessage([CHANNEL, ACTION_NOTIFY, view, id, index]);
-    return 0;
-  }
-  else return notify(view, index, ...rest);
-};
-
-Atomics.waitAsync = (view, ...rest) => {
-  if (views.has(view)) {
-    const [_, value] = waitAsyncPoly(view, ...rest);
-    return { value };
-  }
-  else {
-    return (waitAsync || waitAsyncPatch)(view, ...rest);
+      else return $waitAsync(view, ...rest);
+    };
   }
 };
 
@@ -122,22 +95,8 @@ if (SharedWorker) {
   // did you know? SharedArrayBuffer silently fails
   // when posted to a SharedWorker's port ... so
   // here a polyfill that works regardless w/ SAB
-  const { entries } = Object;
-  const override = value => {
-    if (
-      SAB &&
-      !views.has(value) &&
-      (value instanceof Int32Array || value instanceof BigInt64Array) &&
-      value.buffer instanceof SAB
-    ) {
-      const clone = value.slice(0);
-      const details = [idPlus(), 0, withResolvers()];
-      views.set(value, details);
-      views.set(clone, details);
-      sharedWorker.set(value, clone);
-      return clone;
-    }
-  };
+  const { defineProperties, entries } = Object;
+
   const fix = (data) => {
     const patch = [];
     for (const [key, value] of entries(data)) {
@@ -152,6 +111,23 @@ if (SharedWorker) {
     for (const [key, value] of patch)
       data[key] = value;
   };
+
+  const override = value => {
+    if (
+      !views.has(value) &&
+      (value instanceof Int32Array || value instanceof BigInt64Array) &&
+      !(value.buffer instanceof ArrayBuffer)
+    ) {
+      const clone = value.slice(0);
+      const details = [idPlus(), 0, withResolvers()];
+      // set SAB before clone to fix it once clone is resolved
+      views.set(value, details);
+      views.set(clone, details);
+      sharedWorker.set(value, clone);
+      return clone;
+    }
+  };
+
   const postFixed = (CHANNEL, data) => {
     if (isObject(data)) {
       const overridden = override(data);
@@ -162,8 +138,10 @@ if (SharedWorker) {
     }
     return postData(CHANNEL, data);
   };
+
   SharedWorker = class extends SharedWorker {
     constructor(url, options) {
+      patchAtomics();
       const { port } = super(url, { name: 'sabayon', ...asModule(options) });
       const postMessage = port.postMessage.bind(port);
       addListener(port, 'message', message);
@@ -191,12 +169,47 @@ catch (_) {
   ignore = ignorePatch;
   polyfill = true;
 
+  patchAtomics();
+
   SharedArrayBuffer = class extends ArrayBuffer {}
   BigInt64Array = extend(BigInt64Array, SharedArrayBuffer);
   Int32Array = extend(Int32Array, SharedArrayBuffer);
 
-  let serviceWorker = null;
+  const register = ({ serviceWorker: s }, sw, done) => {
+    let w, c = true;
+    addListener(s, 'message', event => {
+      if (isChannel(event, CHANNEL)) {
+        const [_, id, index] = event.data;
+        const uid = [id, index].join(',');
+        const done = view => {
+          sync.delete(uid);
+          w.postMessage([ CHANNEL, id, index, view ]);
+        };
+        const view = sync.get(uid);
+        if (view) done(view);
+        else {
+          const { promise, resolve } = withResolvers();
+          sync.set(uid, resolve);
+          promise.then(done);
+        }
+      }
+    });
+    // use previous registration, if any, before registering it
+    s.getRegistration(sw)
+      .then(r => (r ?? s.register(sw)))
+      .then(function ready(r) {
+        c = c && !!s.controller;
+        w = (r.installing || r.waiting || r.active);
+        if (w.state === 'activated') {
+          if (c) done();
+          else location.reload();
+        }
+        else
+          addListener(w, 'statechange', () => ready(r), { once: true });
+      });
+  };
 
+  let serviceWorker = null;
   Worker = class extends Worker {
     constructor(url, options) {
       let sw = options?.serviceWorker || '';
