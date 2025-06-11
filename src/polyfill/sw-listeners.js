6383e286b34150769275ee510656f77107260568
webreflection <andrea.giammarchi@gmail.com>
2025-06-11 18:40:41 +0200

    Apparently better on iPad (at least)

--- src/polyfill/sw-listeners.js
+++ src/polyfill/sw-listeners.js
@@ -16,15 +16,40 @@ const bc = new BroadcastChannel(BROADCAST_CHANNEL_UID);
 bc.onmessage = event => resolve.apply(null, event.data);
 
 const url = `${location.href}?sabayon`;
+const options = {
+  headers: {
+    'Content-Type': 'text/plain;charset=utf-8',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Origin': 'Origin',
+  },
+};
 export const fetch = async event => {
   const { request: r } = event;
-  if (r.method === 'POST' && r.url === url) {
+  if (r.url === url) {
     event.stopImmediatePropagation();
     event.preventDefault();
-    const [swid, promise] = next();
-    const resolve = value => new Response(`[${join.call(value, ',')}]`, r);
-    event.respondWith(promise.then(resolve, resolve));
-    const [wid, vid] = parse(await r.text());
-    bc.postMessage([swid, wid, vid]);
+    switch (r.method) {
+      case 'POST': {
+        const [swid, promise] = next();
+        const resolve = value => new Response(
+          `[${join.call(value, ',')}]`,
+          options
+        );
+        event.respondWith(promise.then(resolve, resolve));
+        const [wid, vid] = parse(await r.text());
+        bc.postMessage([swid, wid, vid]);
+        break;
+      }
+      case 'OPTIONS': {
+        event.respondWith(new Response(null, {
+          headers: {
+            'Access-Control-Allow-Methods': 'POST, OPTIONS',
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Allow-Origin': 'Origin',
+          },
+        }));
+        break;
+      }
+    }
   }
 };
