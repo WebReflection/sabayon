551964573778d223c98da37c01f4f40b9fb66b24
webreflection <andrea.giammarchi@gmail.com>
2025-06-11 17:49:50 +0200

    using special headers to see if it solves anything

--- src/polyfill/sw-listeners.js
+++ src/polyfill/sw-listeners.js
@@ -15,14 +15,19 @@ export const install = () => skipWaiting();
 const bc = new BroadcastChannel(BROADCAST_CHANNEL_UID);
 bc.onmessage = event => resolve.apply(null, event.data);
 
-const url = `${location.href}?sabayon`;
+const { href, origin } = location;
+const url = `${href}?sabayon`;
+const headers = new Headers({
+  'Content-Type': 'text/plain;charset=utf-8',
+  'Access-Control-Allow-Origin': origin,
+});
 export const fetch = async event => {
   const { request: r } = event;
   if (r.method === 'POST' && r.url === url) {
     event.stopImmediatePropagation();
     event.preventDefault();
     const [swid, promise] = next();
-    const resolve = value => new Response(`[${join.call(value, ',')}]`, r.headers);
+    const resolve = value => new Response(`[${join.call(value, ',')}]`, headers);
     event.respondWith(promise.then(resolve, resolve));
     const [wid, vid] = parse(await r.text());
     bc.postMessage([swid, wid, vid]);
